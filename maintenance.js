3ea5bcdc6cfea8cd8adf73096490882b7c58e4c6
NattachotLukkanathiti <minec2645@gmail.com>
2026-06-15 11:51:28 +0700

    Update maintenance.js

--- maintenance.js
+++ maintenance.js
@@ -1,4 +1,5 @@
-
+const BASE_URL = "https://mouse-afford-handbrake.ngrok-free.dev"; // เปลี่ยนพอร์ตให้ตรงกับ Backend ของคุณ
+/* ─── CONFIG ─── */
 const TOKEN_KEY  = "access_token";
 const REFRESH_KEY = "refresh_token";
 
